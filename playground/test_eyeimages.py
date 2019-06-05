e457b6ca38612df04275481e4e9abaef4863caee
mnystrom <marcus.nystrom@humlab.lu.se>
2019-06-05 09:31:37 +0200

    more tests with eye images in Python 3

--- playground/test_eyeimages.py
+++ playground/test_eyeimages.py
@@ -11,6 +11,8 @@ import sys
 import base64
 import time
 import tobii_research as tr
+
+em = []
  
 if sys.version_info[0] == 3:
     from tkinter import Tk, PhotoImage
@@ -26,26 +28,27 @@ def eye_image_callback(eye_image_data):
                                          eye_image_data['device_time_stamp'],
                                          eye_image_data['camera_id']))
  
-    print(eye_image_data['image_data'])
-#    image = PhotoImage(data=base64.standard_b64encode(eye_image_data['image_data']))
-#    print("{0} width {1}, height {2}".format(image, image.width(), image.height()))
+    #print(eye_image_data['image_data'])
+    
+    em.append(eye_image_data['image_data'])
+
  
  
 def eye_images(eyetracker):
-    root = Tk()
+    
     print("Subscribing to eye images for eye tracker with serial number {0}.".format(eyetracker.serial_number))
     
     eyetracker.subscribe_to(tr.EYETRACKER_GAZE_DATA, gaze_callback, as_dictionary=True)
     eyetracker.subscribe_to(tr.EYETRACKER_EYE_IMAGES, eye_image_callback, as_dictionary=True)
  
     # Wait for eye images.
-    time.sleep(10)
+    time.sleep(1)
  
     eyetracker.unsubscribe_from(tr.EYETRACKER_EYE_IMAGES, eye_image_callback)
     eyetracker.unsubscribe_from(tr.EYETRACKER_GAZE_DATA, gaze_callback)
     
     print("Unsubscribed from eye images.")
-    root.destroy()
+    
  # <EndExample>
  
  
@@ -53,3 +56,10 @@ eyetracker = tr.EyeTracker('tet-tcp://169.254.10.20')
 print("Address: " + eyetracker.address)
 print("Serial number: " + eyetracker.serial_number)
 execute(eyetracker)
+
+root = Tk()
+for e in em:
+    image = PhotoImage(data=base64.standard_b64encode(e))
+    print("{0} width {1}, height {2}".format(image, image.width(), image.height()))
+    
+root.destroy()    
