4af856bea8bf137633d269d1e1558f002252536c
marcus <marcus.h.nystrom@gmail.com>
2019-09-05 13:03:01 +0200

    added setup file for pip install

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='Titta',
+    url='https://github.com/marcus-nystrom/Titta',
+    author='Marcus Nystrom',
+    author_email='marcus.nystrom@humlab.lu.se',
+    packages=['titta'],
+    install_requires=['psychopy'],
+    version='0.1',
+    license='Creative Commons Attribution 4.0 (CC BY 4.0)',
+    description='Titta is a toolbox for using eye trackers from Tobii with Python, specifically offering integration with PsychoPy',
+    long_description=open('README.md').read(),
+)
