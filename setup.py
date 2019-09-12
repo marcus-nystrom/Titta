d10a049bf787f2f04abc96d05f8e07745cc1f556
mnystrom <marcus.nystrom@humlab.lu.se>
2019-09-12 10:45:21 +0200

    added websocket-client as prerequisite

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author='Marcus Nystrom',
     author_email='marcus.nystrom@humlab.lu.se',
     packages=['titta'],
-    install_requires=['psychopy'],
+    install_requires=['psychopy', 'websocket_client'],
     version='0.1',
     license='Creative Commons Attribution 4.0 (CC BY 4.0)',
     description='Titta is a toolbox for using eye trackers from Tobii with Python, specifically offering integration with PsychoPy',
