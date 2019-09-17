e499bf8863627086fa80ff0b59ef6f56bcdba930
mnystrom <marcus.nystrom@humlab.lu.se>
2019-09-17 14:09:21 +0200

    started implemented a dummy class for the Lab integration

---
+++ titta/TalkToProLab_dummy.py
@@ -0,0 +1,468 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  6 14:37:06 2018
+
+@author: Marcus
+ToDo: upload information in chunks of 64 KB
+"""
+
+# import relevant libs
+from websocket import create_connection
+import json
+import time
+import numpy as np
+
+    
+
+
+class TalkToProLab(object):
+    """ A dummy class for talking to Tobii Pro Lab. 
+    """     
+    
+    #%%
+    def __init__(self, project_name=None):
+        
+        print('Dummy class init')
+
+            
+    #%% 
+    def send_message(self, address, msg_dict, to_json=True):
+        ''' Send message to server and return response
+        '''
+        
+        print('send_message')
+    
+        return None
+
+    #%%
+    def get_api_version(self):
+        ''' Get version of api 
+         Available for all Services. 
+        request:
+        {"operation": "GetApiVersion"}
+        
+        response:
+        {"operation": "GetApiVersion",
+        "status_code": 0,
+        "version": "1.0"}        
+        '''
+        print('get_api_version')
+
+        return None        
+        
+    #%%          
+    def get_time_stamp(self):
+        ''' Clock API
+        request:
+        {"operation": "GetTimestamp"}
+        
+        response:
+        {"operation": "GetTimestamp",
+        "status_code": 0,
+        "timestamp": "128606207528320000"}
+        
+        Note: timestamp in microseconds
+        '''
+        
+        print('get_time_stamp')
+
+        return None  
+    #%%
+    def add_participant(self, participant_name):
+        ''' Project data API
+        request:
+        {"operation": "AddParticipant",
+        "participant_name": "John Doe"}
+        
+        response:
+        {"operation": "AddParticipant",
+        "status_code": 0,
+        "participant_id": "2A86C895-F8B6-4786-8C4B-CB889C3449F1"}
+        '''   
+        
+        print('add_participant')
+
+        return None    
+        
+    #%%
+    def get_project_info(self):
+        ''' Project data API
+        request:
+        { "operation": "GetProjectInfo"}
+        
+        response:
+        {"operation": "GetProjectInfo",
+        "status_code": 0,
+        "project_id": "8928673C-B1D5-4941-A592-39597742B1E5",
+        "project_name": "SocialInteraction test"}
+        '''    
+        print('get_project_info')
+
+        return None       
+    
+    #%%
+    def find_participant(self, participant_name):
+        ''' Finds whether participant_name already has been uploaded to lab
+        
+        Args:
+            participant_name - string with participant name (e.g., P04)
+        
+        Returns:
+            exists - boolean 
+        '''
+        
+        print('find_participant')
+        
+        return False
+    
+    #%% 
+    def list_participants(self):
+        ''' Project data API
+        Returns information about the participants of the currently opened project in Pro Lab.
+        
+        request:
+        {
+        "operation": "ListParticipants"
+        } 
+        response:
+        {
+        "operation": "ListParticipants",
+        "status_code": 0,
+        "participant_list": [{
+        "participant_name": "John Doe",
+        "participant_id": "2A86C895-F8B6-4786-8C4B-CB889C3449F1"
+        }]
+        }        
+        '''
+        
+        print('find_participant')
+        
+        return None          
+    
+    #%% 
+    def list_media(self):
+        ''' Project data API
+        Returns information about the uploaded media of the currently opened project in Pro Lab.
+        
+        request:
+        {
+        "operation": "ListMedia"
+        }        
+        
+        response:
+        {
+        "operation": "ListMedia",
+        "status_code": 0,
+        "media_list": [{
+            "media_name": "CuteCats",
+            "media_id": "b2548e0e-4056-4d36-a4f5-86b3194866f7",
+            "mime_type": "image/jpeg",
+            "media_size": 3536263,
+            "width": 2512,
+            "height": 1884,
+            "duration": 0,
+        },
+        {
+            "media_name": "CuteCatsRunningAround",
+            "media_id": "21BA2272-473D-478F-9E23-A1FA8ABFF65D",
+            "md5_checksum": "0dbc22fddaff860ac1bf0d092c909d4e",
+            "mime_type": "video/mp4",
+            "media_size": 3536263,
+            "width": 640,
+            "height": 480,
+            "duration": 5.250
+        }]
+        }   
+        '''
+        
+        print('list_media')
+        
+        return None   
+       
+        
+    #%%
+    def find_media(self, media_name):
+        ''' Finds whether media_name already has been uploaded to lab
+        
+        Args:
+            media_name - string with media name (e.g., image.png)
+        
+        Returns:
+            exists - boolean 
+        '''
+        
+        print('find_media')
+        
+        return None
+        
+    #%%
+    def upload_media(self, media_name, media_type):
+        ''' 
+        
+        Prepares Server to receive media content (image or video) as binary data.
+        The response will be sent once immediately after request and once at the end of the upload
+        operation, regardless the reason of upload operation completion. After a success response to an
+        initial request, chunked binary data is expected to be sent.
+        The whole operation of receiving media could be ended due to the following reasons:
+            
+        1. The Server received an amount of binary data equal to media_size parameter.
+        The operation has been aborted by  
+        2. UploadMediaAbort request.
+        3. The time between the Server received two consecutive chunks exceeded timeout.  
+            -> status 103
+            
+        Args:
+            media_name - name of media.ext (e.g., image.png)
+            media_type - 'image' or 'video'
+            
+        
+        
+        -----        
+        Project data API
+        request:
+        {"operation": "UploadMedia",
+        "mime_type": "video/mp4",
+        "media_name": "SocialInteractionDemoVideo.mp4",
+        "media_size": "37201059"}
+        
+        response:
+        {"operation": "UploadMedia",
+        "status_code": 0}
+        '''   
+        
+        # File extension
+        file_ext = media_name.split('.')[1]
+        
+        # Check that media format is supported
+        assert(len([i for i in ['bmp','jpeg', 'png','gif','mp4', 'x-msvideo'] \
+                    if file_ext == i]) > 0), "File type not supported"
+        
+        print('upload_media')
+        
+        return None
+          
+    #%% TODO: remove?
+    def upload_media_abort(self):
+        ''' Project data API
+        request:
+        {"operation": "UploadMediaAbort"}
+        
+        response:
+        {"operation": "UploadMediaAbort",
+        "status_code": 0}
+        '''    
+                
+        print('upload_media_abort')
+        
+        return None
+ 
+    #%%
+    def add_aois_to_image(self, media_id, aoi_name, aoi_color, 
+                  key_frame_vertices, key_frame_active = True, 
+                  key_frame_seconds = 0, tag_name = '',
+                  group_name='', merge_mode='replace_aois'):
+        
+        
+        ''' Project data API
+        request:
+    
+        {
+        "operation": "AddAois",
+        "media_id": "b3418558-b245-4185-9bdc-725fb6a23a88",
+        "aois":  {'name:' : 'test',
+                  'color' : 'AACC33',
+                  'key_frames' : {'is_active' : true,
+                                  'seconds' : 0,
+                                  'vertices' : {{100, 100},
+                                                {100, 200},
+                                                {200, 200},
+                                                {200, 100}}},
+                  'tags': {'tag_name' : 'vehicle',
+                          'group_name' : 'Brand'}},
+        "merge_mode": "ReplaceAois"
+        }  
+                
+        response:
+        {
+        "operation": "sendAois",
+        "imported_aoi_count": 1
+        }            
+        ''' 
+        
+        print('add_aois_to_image')
+        
+        return None
+    
+   #%%
+    def add_aois_to_video(self, media_id, aoi_name, aoi_color, 
+                  key_frame_vertices, key_frame_active = True, 
+                  key_frame_seconds = 0, tag_name = '',
+                  group_name='', merge_mode='replace_aois'):
+        ''' 
+        
+        Placeholder for adding AOIs to video. RTFM!
+        
+        Project data API
+        request:
+    
+        {
+        "operation": "AddAois",
+        "media_id": "b3418558-b245-4185-9bdc-725fb6a23a88",
+        "aois":  {'name:' : 'test',
+                  'color' : 'AACC33',
+                  'key_frames' : {'is_active' : true,
+                                  'seconds' : 0,
+                                  'vertices' : {{100, 100},
+                                                {100, 200},
+                                                {200, 200},
+                                                {200, 100}}},
+                  'tags': {'tag_name' : 'vehicle',
+                          'group_name' : 'Brand'}},
+        "merge_mode": "ReplaceAois"
+        }  
+                
+        response:
+        {
+        "operation": "sendAois",
+        "imported_aoi_count": 1
+        }            
+        '''     
+    #%%
+    def get_state(self):
+        ''' External presenter API.
+        Returns the current state of External Presenter workflow.
+        
+        request:
+        {
+        "operation": "GetState"
+        }
+        response:
+        {
+        "operation": "GetState",
+        "status_code": 0,
+        "state": "unmet"
+        }        
+        '''
+		
+        print('get_state')
+        
+        return None
+
+    #%%           
+    def start_recording(self, recording_name, participant_id, screen_width,
+                        screen_height, screen_latency=10000):
+        ''' External presenter API     
+        request:
+        {"operation": "StartRecording",
+        "recording_name": "Waterfall",
+        "participant_id": "3E6C3751-389A-47F0-861D-7A09D744FC23",
+        "screen_latency": 10000,
+        "screen_width": 1024,
+        "screen_height": 768}
+        
+        response:
+        {"operation": "StartRecording",
+        "status_code": 0,
+        "recording_id": "4B7F3751-389A-47D0-861D-7A09D744FC12"}
+        
+        Note: Participant identifier (returned in
+                                      AddParticipant operation
+                                      response)
+        '''
+        
+        print('start_recording')
+        
+        return None
+    #%%           
+    def stop_recording(self):
+        ''' External presenter API     
+        request:
+        {"operation": "StopRecording"}
+        
+        response:
+        {"operation": "StopRecording",
+        "status_code": 0}
+        '''   
+        
+        
+        print('stop_recording')
+        
+        return None
+        
+    #%%           
+    def send_stimulus_event(self, recording_id, start_timestamp, 
+                            media_id, media_position=None, background=None,
+                            end_timestamp=None):
+                            
+        ''' External presenter API.
+        Timestamps should be in microseconds
+        
+        request:
+        {"operation": "SendStimulusEvent",
+        "recording_id": "4B7F3751-389A-47D0-861D-7A09D744FC12",
+        "start_timestamp": "123123123123213123",
+        "end_timestamp": "321321321321321321",
+        "media_id": "2A86C895-F8B6-4786-9C4B-CB889C3449F0",
+        "media_position":  {"left": 10,
+                            "top": 20,
+                            "right": 850,
+                            "bottom": 1100},
+        "background": "AA91C0"}
+        
+        response:
+        {"operation": "SendStimulusEvent",
+        "status_code": 0}
+        '''       
+        
+        print('send_stimulus_event')
+        
+    #%%           
+    def send_custom_event(self):
+        ''' External presenter API     
+        request:
+        {"operation": "SendCustomEvent",
+        "recording_id": "4B7F3751-389A-47D0-861D-7A09D744FC12",
+        "timestamp": "123123123123213123",
+        "event_type": "ERROR_SEM_OWNER_DIED",
+        "value": "105 (0x69)"}
+        
+        response:
+        {"operation": "SendCustomEvent",
+        "status_code": 0}
+        '''          
+    #%%           
+    def finalize_recording(self, recording_id):
+        ''' Finalizes the recording and makes it ready for analysis in the 
+        application.
+        
+        If validation of data was sent, Server responds with status code 201. 
+        Once the root cause of a validation failure has been fixed, 
+        the command can be executed again.        
+        
+        External presenter API.  
+        request:
+        {"operation": "FinalizeRecording",
+        "recording_id": "4B7F3751-389A-47D0-861D-7A09D744FC12"}
+        
+        response:
+        {"operation": "FinalizeRecording",
+        "status_code": 0}
+        
+        Note: recording_id is returned from start_recording
+        '''             
+        
+        print('finalize_recording')
+        
+        return None
+        
+        
+    #%% 
+    def disconnect(self):
+        ''' Closes the websocket connection
+        '''
+        print('disconnect')
+        
+        return None
+        
+        
+        
