6a85719a889112162bc7ee81a99c3874633fcf1d
marcus <marcus.h.nystrom@gmail.com>
2020-01-24 08:33:51 +0100

    updated example of when python threading may be a problem when pulling samples from the sdk

--- resources/test_temporal_precision.py
+++ resources/test_temporal_precision.py
@@ -1,8 +1,17 @@
+'''
+Since samples are pulled in callbacks with the Tobii SDK, it may get 
+hick-ups if your script is doing something very computationally heavy, 
+without allowing significant sleeps (which would allow the callback to 
+be called and therefore all sample to be collected appropriately).
+
+This can be tested in a while-loop like the one below.                                     
+'''
 # Import modules
 import pickle
 import pandas as pd
 import numpy as np
 import time
+from psychopy import core
 from titta import Titta, helpers_tobii as helpers
 import matplotlib.pyplot as plt
 
@@ -30,45 +39,39 @@ tracker.init()
     
 #%% Record some data
 tracker.start_recording(gaze_data=True, store_data=True)
-
-n_samples = 600 * 2
+core.wait(0.2)
+n_samples = 600 * 2 # Record two seconds of data at 600 Hz
 k = 0
 
 out = []
 ts_old = 0
 
 t0 = time.clock()
-#time.sleep(2)
 call_times = []
 while k < n_samples:
     
-    t1 = time.clock()
+#    t1 = time.clock()
     s = tracker.get_latest_sample()
     ts = s['system_time_stamp']
-    call_times.append(time.clock() - t1)
 
     if ts == ts_old:
-        time.sleep(0.011)
+        core.wait(0.00001) # Wait 1/10 ms
+#        call_times.append(time.clock() - t1)
         continue
     
     out.append([time.clock(), ts])
     k += 1
     ts_old = ts
     
-#    time.sleep(0.1)
-# 
 print(time.clock() - t0)
 tracker.stop_recording(gaze_data=True)
 
 out = np.array(out)
 
-#%%
-
+#%% Plot data capture in real time (tobii time stamps, and loop intervals)
 plt.plot(np.diff(out[:, 0] * 1000))    
 plt.figure()
 plt.plot(np.diff(out[:, 1] / 1000))    
-
-
 
 #%%
 ut =[]
