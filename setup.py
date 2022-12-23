117e95acbf3403e4c93df7accc786801ae2d9d3a
marcus <marcus.h.nystrom@gmail.com>
2022-12-23 09:56:30 +0100

    bumped version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     author_email='marcus.nystrom@humlab.lu.se',
     packages=['titta'],
     install_requires=['psychopy', 'websocket_client'],
-    version='0.1',
+    version='1.0',
     license='Creative Commons Attribution 4.0 (CC BY 4.0)',
     description='Titta is a toolbox for using eye trackers from Tobii with Python, specifically offering integration with PsychoPy',
     long_description=open('README.md').read(),
