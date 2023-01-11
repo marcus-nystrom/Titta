e4ccab37f0bf35a32debc574c5df377da084a8a9
mnystrom <marcus.nystrom@humlab.lu.se>
2023-01-11 09:20:27 +0100

    updated to PyTitta format

--- playground/et_head_test.py
+++ playground/et_head_test.py
@@ -11,13 +11,13 @@ import tobii_research
 # Insert the parent directory (where Titta is) to path
 from titta import Titta, helpers_tobii as helpers
 
- 
+
 win = visual.Window(screen=1)
 
 # Parameters
 et_name = 'Tobii Pro Spectrum'
 dummy_mode = False
-    
+
 # Change any of the default dettings?
 settings = Titta.get_defaults(et_name)
 settings.FILENAME = 'testfile.tsv'
@@ -30,29 +30,28 @@ tracker.init()
 
 # Start streaming of eye images
 tracker.start_recording(gaze=True, positioning=True)
-    
+core.wait(1)
 et_head = helpers.EThead(win)
 
 while 1:
 #try:
-    sample = tracker.buffer.peekN('gaze', 1)
-    sample_user_position = tracker.buffer.peekN('positioning', 1)
-
+    sample = tracker.buffer.peek_N('gaze', 1)
+    sample_user_position = tracker.buffer.peek_N('positioning', 1)
     latest_valid_bincular_avg, \
     previous_binocular_sample_valid,\
     latest_valid_yaw, \
     latest_valid_roll, \
     offset = et_head.update(sample, sample_user_position)
- 
+
     et_head.draw()
     win.flip()
 
     k = event.getKeys()
     if 'escape' in k:
-        tracker.stop_recording(gaze=True)
+        tracker.stop_recording(gaze=True, positioning=True)
         win.close()
         break
-    
+
 tracker.stop_recording(gaze=True, positioning=True)
 win.flip()
 win.close()
