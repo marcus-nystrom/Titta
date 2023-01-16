4170f36a62408d31fa3d49cec75c19fd122ece16
mnystrom <marcus.nystrom@humlab.lu.se>
2023-01-16 21:10:01 +0100

    added correct sample structure

--- titta/Tobii_dummy.py
+++ titta/Tobii_dummy.py
@@ -10,165 +10,247 @@ import time
 from titta import helpers_tobii as helpers
 from threading import Thread
 import numpy as np
+import sys
 
-        
-class Connect(Thread):
+sample_list = ['device_time_stamp', 'system_time_stamp',
+       'left_gaze_on_display_area_x',
+       'left_gaze_on_display_area_y',
+       'left_gaze_point_in_user_coordinates_x',
+       'left_gaze_point_in_user_coordinates_y',
+       'left_gaze_point_in_user_coordinates_z', 'left_gaze_point_valid',
+       'left_gaze_point_available', 'left_pupil_diameter', 'left_pupil_valid',
+       'left_pupil_available', 'left_gaze_origin_in_user_coordinates_x',
+       'left_gaze_origin_in_user_coordinates_y',
+       'left_gaze_origin_in_user_coordinates_z',
+       'left_gaze_origin_in_track_box_coordinates_x',
+       'left_gaze_origin_in_track_box_coordinates_y',
+       'left_gaze_origin_in_track_box_coordinates_z', 'left_gaze_origin_valid',
+       'left_gaze_origin_available', 'left_eye_openness_diameter',
+       'left_eye_openness_valid', 'left_eye_openness_available',
+       'right_gaze_on_display_area_x',
+       'right_gaze_on_display_area_y',
+       'right_gaze_point_in_user_coordinates_x',
+       'right_gaze_point_in_user_coordinates_y',
+       'right_gaze_point_in_user_coordinates_z', 'right_gaze_point_valid',
+       'right_gaze_point_available', 'right_pupil_diameter',
+       'right_pupil_valid', 'right_pupil_available',
+       'right_gaze_origin_in_user_coordinates_x',
+       'right_gaze_origin_in_user_coordinates_y',
+       'right_gaze_origin_in_user_coordinates_z',
+       'right_gaze_origin_in_track_box_coordinates_x',
+       'right_gaze_origin_in_track_box_coordinates_y',
+       'right_gaze_origin_in_track_box_coordinates_z',
+       'right_gaze_origin_valid', 'right_gaze_origin_available',
+       'right_eye_openness_diameter', 'right_eye_openness_valid',
+       'right_eye_openness_available']
+
+class Buffer(Thread):
     """
-    Create a class that simplifies life for people wanting to use the SDK
+    Create that simulates a data buffer
     """
-    def __init__(self):
-        
-        # clock
-        self.clock = core.Clock()
-        
-        # Dict to store samples
-        self.sample = {}
-     
-    def init(self):
-        ''' Connect to eye tracker
-        and apply settings
-        '''
-        
-        self.__stop = True
-                
-    #%%    
-    def is_connected(self):
-        print('is connected')      
-    
-    #%% Init calibration
-    def calibrate(self, win, eye='both', calibration_number='first'):
-        ''' Master function for setup and calibration
-        '''
-        
-        # Window and instruction text for calibration        
-        instruction_text = visual.TextStim(win,text='',wrapWidth = 1,
-                                           height = 0.05, units='norm')  
-        
-        self.instruction_text = instruction_text
-        
+    def __init__(self, win):
+
         self.win = win
-        
+
         # Mouse object
-        self.mouse = event.Mouse()    
+        self.mouse = event.Mouse(win)
         self.mouse.setPos([0,0])
         self.mouse.setVisible(0)
-        
-        self.instruction_text.text = 'Calibration Dummy mode'
-        self.instruction_text.draw()
-        self.win.flip()
-        core.wait(2)
-        
-    #%%   
-    def get_system_time_stamp(self):
-        ''' Get system time stamp
-        '''
-        
-        print('time_stamp')          
-                
-    #%%            
-    def start_recording(self,   gaze_data=False, 
-                                sync_data=False,
-                                image_data=False,
-                                stream_errors=False,
-                                external_signal=False,
-                                user_position_guide=False,
-                                store_data=True):
-        print('start_recording')      
-        
-    #%% 
-    def start_sample_buffer(self, sample_buffer_length=3):  
-        
+
+        self.__stop = True
+        self.sample = {}
+
+        # Initiate an empty sample dict
+        for l in sample_list:
+            self.sample[l] = [0]
+
+        self._start_sample_buffer()
+
+    #%%
+    def peek_N(self, stream, N):
+        ''' Consume all samples and empty buffer'''
+        return self.sample
+
+    #%%
+    def consume_N(self, stream, N):
+        ''' Get samples in buffer while emptying the buffer '''
+
+        temp = self.sample
+        self.sample = {}
+        return temp
+
+    #%%
+    def _start_sample_buffer(self, sample_buffer_length=sys.maxsize):
+
         Thread.__init__(self)
-        
+
         # Initialize the ring buffer
-        self.buf = helpers.RingBuffer(maxlen=sample_buffer_length)
         self.__stop = False
-        self.start()   
-        
-        
-    #%%             
+        self.start()
+
+
+    #%%
     def run(self):
         # Called by the e.g., et.start()
         # Continously read data into a ringbuffer
         while True:
-            if self.__stop:        
+            if self.__stop:
                 break
-            
-            # Get samples and store in ringbuffer  
-            sample = self.get_latest_sample()
-            
-            self.buf.append(sample)
-            time.sleep(0.01)            
-        
-    #%% 
-    def send_message(self, msg, ts=None):
-        print(msg)      
-        
+
+            # Add sample to dict
+            self._add_sample_to_buffer()
+            time.sleep(0.01)
+
     #%%
-    def get_latest_sample(self):
+    def _add_sample_to_buffer(self):
         ''' Simulates gaze position with mouse
-        ToDO use same struct as gaze data
         '''
         x, y = self.mouse.getPos()
-        
+
         # Convert mouse position in current psychopy units to Tobii's
         # norm coordinate system (currently supports 'norm' and 'deg')
-        
+
         if self.win.units == 'norm':
             xy = helpers.norm2tobii(np.array([x, y],ndmin=2))
         elif self.win.units == 'deg':
-            xy = helpers.deg2tobii(np.array([x, y],ndmin=2), self.win.monitor)    
+            xy = helpers.deg2tobii(np.array([x, y],ndmin=2), self.win.monitor)
         elif self.win.units == 'pix':
-            xy = helpers.pix2tobii(np.array([x, y],ndmin=2), self.win.monitor)            
+            xy = helpers.pix2tobii(np.array([x, y],ndmin=2), self.win.monitor)
         else:
             raise IOError ('Invalid unit of PsychoPy screen: Titta in dummy mode currently \
-                           supports "norm", "pix", and "deg".')            
-        
-        self.sample['left_gaze_point_on_display_area'] = (xy[0, 0], xy[0, 1])
-        self.sample['right_gaze_point_on_display_area'] = (xy[0, 0], xy[0, 1])
-        
-        return self.sample
-        
+                           supports "norm", "pix", and "deg".')
+        for key in self.sample.keys():
+            if '_x' in key:
+                self.sample[key].append(xy[0, 0])
+            elif '_y':
+                self.sample[key].append(xy[0, 1])
+            else:
+                self.sample[key].append(np.random.rand())
+
     #%%
-    def consume_buffer(self):
-        ''' Consume all samples and empty buffer'''
-        return self.buf.get_all() 
-    
-    #%%
-    def peek_buffer(self):
-        ''' Get samples in buffer without emptying the buffer '''
-        return self.buf.peek()  
-    
-    #%% 
-    def stop_sample_buffer(self):
+    def _stop_sample_buffer(self):
         self.__stop = True
-        
-    #%%    
-    def stop_recording(self,    gaze_data=False, 
-                                sync_data=False,
-                                image_data=False,
-                                stream_errors=False,
-                                external_signal=False,
-                                user_position_guide=False):
-        print('stop_recording')      
-    #%% 
-    def save_data(self, *argv, filename=None):
-        print('save_data')      
-        
+##########################
+##########################
+##########################
+##########################
+
+# %%
+class Connect(object):
+    """
+    Create a class that simplifies life for people wanting to use the SDK
+    """
+    def __init__(self):
+
+        # clock
+        self.clock = core.Clock()
+
+        # Dict to store samples
+
+    def init(self):
+        ''' Connect to eye tracker
+        and apply settings
+        '''
+        print('init')
+
+
+    #%% Init calibration
+    def calibrate(self, win, eye='both', calibration_number='first'):
+        ''' Master function for setup and calibration
+        '''
+        self.buffer = Buffer(win)
+
+        # Window and instruction text for calibration
+        instruction_text = visual.TextStim(win,text='',wrapWidth = 1,
+                                           height = 0.05, units='norm')
+
+        self.instruction_text = instruction_text
+
+        self.win = win
+
+        self.instruction_text.text = 'Calibration Dummy mode'
+        self.instruction_text.draw()
+        self.win.flip()
+        core.wait(2)
+
     #%%
-    def de_init(self):
-        self.mouse.setVisible(1)
-        print('de_init')      
-    
-    #%% 
+    def get_system_time_stamp(self):
+        ''' Get system time stamp
+        '''
+
+        print('time_stamp')
+
+    #%%
+    def start_recording(self,   gaze=False,
+                                time_sync=False,
+                                eye_image=False,
+                                notifications=False,
+                                external_signal=False,
+                                positioning=False):
+        print('start_recording')
+
+
+
+    #%%
+    def send_message(self, msg, ts=None):
+        print(msg)
+
+
+    #%%
+    def stop_recording(self,    gaze=False,
+                                time_sync=False,
+                                eye_image=False,
+                                notifications=False,
+                                external_signal=False,
+                                positioning=False):
+        print('stop_recording')
+    #%%
+    def save_data(self, *argv, filename=None):
+        print('save_data')
+
+    #%%
     def set_dummy_mode(self):
-        print('Does nothing')
+        print('dummy mode')
 
-    #%%     
+    #%%
     def calibration_history(self):
-        print('Does calibration_history')
+        print('calibration_history')
         return None
-    
+    #%%
+    def system_info(self):
+        ''' Returns information about system in dict
+        '''
 
-   
- 
+        info = {}
+        info['serial_number']  = 'dummy'
+        info['address']  = 'dummy'
+        info['model']  = 'dummy'
+        info['name']  = 'dummy'
+        info['firmware_version'] = 'dummy'
+        info['runtime_version'] = 'dummy'
+        info['tracking_mode']  = 'dummy'
+        info['sampling_frequency']  = 'dummy'
+        info['track_box']  = 'dummy'
+        info['display_area']  = 'dummy'
+        info['python_version'] = '.'.join([str(sys.version_info[0]),
+                                           str(sys.version_info[1]),
+                                           str(sys.version_info[2])])
+        info['psychopy_version'] = 'dummy'
+        info['TittaPy_version'] = 'dummy'
+        info['titta_version'] = 'dummy'
+
+    #%%
+    def set_sample_rate(self, Fs):
+        '''Sets the sample rate
+        '''
+        print('set sample rate')
+    #%%
+    def get_sample_rate(self):
+        '''Gets the sample rate
+        '''
+        print('get sample rate')
+        return 0
+
+
+
+
