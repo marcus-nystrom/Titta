371886ead920970d9b5cb6db8c14b566ebf0da33
mnystrom <marcus.nystrom@humlab.lu.se>
2023-01-24 18:14:54 +0100

    clean-up of playground folder

--- playground/test_lost_samples_titta.py
+++ playground/test_lost_samples_titta.py
@@ -1,13 +1,13 @@
 '''
-Since samples are pulled in callbacks with the Tobii SDK, it may get
+Since samples are pulled in callbacks with the Tobii SDK in Python, it may get
 hick-ups if your script is doing something very computationally heavy,
 without allowing significant sleeps (which would allow the callback to
 be called and therefore all sample to be collected appropriately).
 
 This can be tested in a while-loop like the one below.
 
-If in a while loop without a time.sleep() or core.wait(), data are lost!
-IMPORTANT:  Make sure your script always have a core.wait() or win.flip in each loop.
+Pulling samples with PyTitta (C++ wrapper) doesn't show this problem luckily, so the below loop is OK!
+
 '''
 # Import modules
 import numpy as np
@@ -23,7 +23,7 @@ et_name = 'Tobii Pro Spectrum'
 
 # Change any of the default dettings?e
 settings = Titta.get_defaults(et_name)
-settings.FILENAME = 'testfile.tsv'
+settings.FILENAME = 'testfile'
 
 #%% Connect to eye tracker and calibrate
 tracker = Titta.Connect(settings)
@@ -32,22 +32,11 @@ tracker.init()
 
 #%% Record some data
 
-'''
-Since samples are pulled in callbacks with the Tobii SDK, it may get
-hiccups if your script is doing something very computationally heavy,
-without allowing significant sleeps (which would allow the callback to
-be called and therefore all samples to be collected appropriately).
-
-This can be tested in a while-loop like the one below.
-
-TobiiWrapper doesn't show this problem luckily, so the below loop is OK!
-'''
 dur = 4 # Record what should be this many seconds of data
-n_samples = tracker.frequency * dur
+n_samples = int(tracker.get_sample_rate() * dur)
 
 out = []
 k = 0
-ts = 0
 ts_old = 0
 
 tracker.start_recording(gaze=True,
@@ -58,24 +47,27 @@ tracker.start_recording(gaze=True,
                             positioning=True)
 
 t0 = time.perf_counter()
+out = np.zeros((n_samples, 2))
 while k < n_samples:
-    samples = tracker.peek_N('gaze')
-    if len(samples)>0:
-        ts = samples['system_time_stamp'][0]
 
-    if ts == ts_old:
-        #core.wait(0.00001) # Wait 1/10 ms
+    # Grab teh most recent sample
+    samples = tracker.buffer.peek_N('gaze', 1, 'end')
+
+    # is there a sample?
+    if len(samples['system_time_stamp']) > 0:
+        ts = samples['system_time_stamp'][0]
+    else:
         continue
 
-    out.append([time.perf_counter(), ts])
+    # is it a new sample?
+    if ts == ts_old:
+        continue
+
+    out[k, 0] = time.perf_counter()
+    out[k, 1] = ts
+
     k += 1
     ts_old = ts
-
-    if out[-1][0]-t0>dur/2:
-        # start recording eye openness. Also, if set to true, any
-        # calls to start or stop either gaze or eye_openness will
-        # start or stop both
-        tracker.set_include_eye_openness_in_gaze(True)
 
 print(time.perf_counter() - t0)
 tracker.stop_recording(gaze=True,
@@ -87,14 +79,13 @@ tracker.stop_recording(gaze=True,
 
 
 #%% Plot data captured in real time (tobii time stamps, and loop intervals)
-out = np.array(out)
 plt.figure()
 plt.plot(np.diff(out[:, 0] * 1000))
 plt.figure()
 plt.plot(np.diff(out[:, 1] / 1000))
 
 #%% Plot timestamps of samples in the buffer
-all_samples = tracker.peek_N('gaze', sys.maxsize)
+all_samples = tracker.buffer.consume_N('gaze', sys.maxsize)
 
 plt.figure()
 plt.plot(np.diff(all_samples['system_time_stamp']) / 1000)
