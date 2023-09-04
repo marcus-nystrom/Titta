ef16faf8943e4ebe46981e3b31a0282f2b311209
mnystrom <marcus.nystrom@humlab.lu.se>
2023-09-04 09:37:30 +0200

    re-added minimal example

---
+++ demo_experiment/read_me_minimal.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  4 09:36:50 2023
+
+@author: Marcus
+"""
+
+# Import relevant modules
+from psychopy import visual, core
+from titta import Titta
+
+# Create an instance of titta
+et_name = 'Tobii Pro Spectrum'
+settings = Titta.get_defaults(et_name)
+settings.DATA_STORAGE_PATH = 'c:\Humlab'
+
+# Connect to eye tracker and calibrate
+tracker = Titta.Connect(settings)
+tracker.init()
+
+# Window set-up (this color will be used for calibration)
+win = visual.Window(size=(1920, 1080))
+tracker.calibrate(win)
+
+# Start recording
+tracker.start_recording(gaze=True)
+
+# Show your stimuli
+win.flip()
+core.wait(1)
+
+# Stop recording
+tracker.stop_recording(gaze=True)
+
+# Close window and save data
+win.close()
+tracker.save_data()
