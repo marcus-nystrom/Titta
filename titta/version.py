0821b5ec8d72ef127781563c2e1dc366acdc6d66
mnystrom <marcus.nystrom@humlab.lu.se>
2023-11-13 11:17:31 +0100

    version bump

--- titta/version.py
+++ titta/version.py
@@ -1,4 +1,4 @@
-__version__ = "2.0.3"
+__version__ = "2.0.4"
 
 __title__ = "titta"
 __description__ = "Titta is a toolbox for using eye trackers from Tobii with Python, specifically offering integration with PsychoPy."
