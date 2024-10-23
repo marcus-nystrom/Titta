c2a66386dfdfca1261db61058e561b584ef243bd
dev-jam <mail@bobrosbag.nl>
2024-10-24 00:21:45 +0200

    check if OpenCV can be imported for image processing, if not then use PsychoPy

--- demo_analyses/plot_scanpath.py
+++ demo_analyses/plot_scanpath.py
@@ -8,21 +8,20 @@ Created on Mon Sep  5 11:06:43 2022
 
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
 import os
-import cv2
 import time
+
+try:
+    import cv2
+except ImportError:
+    print("Could not import OpenCV for image processing, using PsychoPy instead")
+    from psychopy import visual, core
+    use_cv2 = False
+else:
+    use_cv2 = True
+
 start = time.time()
-
-
-# %%
-
-def add_transparency_cv2(overlay, image, alpha):
-    ## image used in func must be a copy of image! The image from image can be original, original is fastest
-    if alpha == 1:
-        image_new = overlay
-    else:
-        image_new = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
-    return image_new
 
 # %%
 
@@ -40,11 +39,105 @@ def make_scanpath(image_name, fixations, imres, scale_with_duration=True):
         None.
 
     '''
+
+    win = visual.Window(fullscr=True, screen=1, units='pix', size=imres)
+
+    dot = visual.Circle(win, radius=50, lineColor='red', fillColor='red',
+                        opacity=0.5)
+    line = visual.Line(win, start=(-0.5, -0.5), end=(0.5, 0.5),
+                        lineWidth=3, lineColor='red', opacity=0.5)
+
+    im = visual.ImageStim(win, image=str(image_name), size=(imres[0], imres[1]))
+    im.draw()
+
+    start_pos_old = (None, None)
+    for i, row in fixations.iterrows():
+        dot.pos = (row.xpos - imres[0]/2, imres[1]/2 - row.ypos)
+
+
+
+        if i == 0:
+            dot.fillColor = 'green'
+            dot.lineColor = 'green'
+        elif i == len(fixations)-1:
+            dot.fillColor = 'red'
+            dot.lineColor = 'red'
+            line.lineColor = 'red'
+
+        else:
+            dot.fillColor = 'blue'
+            dot.lineColor = 'blue'
+            line.lineColor = 'blue'
+
+
+        line.end = dot.pos
+
+        if i > 0:
+            if i < 2:
+                line.lineColor = 'green'
+
+            line.start = start_pos_old
+            line.draw()
+
+        dot.opacity=0.5
+
+
+        if scale_with_duration:
+            dot.radius = row.dur/10
+
+        dot.draw()
+            # print(row.xpos, row.ypos, dot.radius)
+
+        start_pos_old = line.end
+
+
+    win.flip()
+    core.wait(1)
+
+    win.getMovieFrame()
+
+    # Make a new dir to save plots if it does not already exists
+    path = Path.cwd() / 'scanpaths' / str(fixations.participant[0])
+    Path(path).mkdir(parents=True, exist_ok=True)
+
+    # Save the results
+    fname =  path / ('scanpath_' + str(image_name).split(os.sep)[-1])
+    win.saveMovieFrames(fname)
+    win.close()
+
+# %%
+
+# %%
+
+def add_transparency_cv2(overlay, image, alpha):
+    ## image used in func must be a copy of image! The image from image can be original, original is fastest
+    if alpha == 1:
+        image_new = overlay
+    else:
+        image_new = cv2.addWeighted(overlay, alpha, image, 1 - alpha, 0)
+    return image_new
+
+# %%
+
+def make_scanpath_cv2(image_name, fixations, imres, scale_with_duration=True):
+    '''
+
+
+    Args:
+        image (str): name to image (full path)
+        fixations (dataframe): with fixations for the images with columns 'xpos' (pixels), 'ypos' (pixels), 'dur' (ms)
+        imres (tuple) resoluiton of image in pixels
+        scale_with_duration (bool, optional): Scale fixations with durations (otherwise all fixations will have the same size)
+
+    Returns:
+        None.
+
+    '''
     alpha = 0.5
     dot_radius = 50
     thickness = 2
 
-    im_rgb = cv2.imread(image_name)
+    im_rgb = cv2.imread(str(image_name))
     image = im_rgb.copy()
     start_pos_old = (None, None)
     for i, row in fixations.iterrows():
@@ -106,7 +199,10 @@ for participant in list(participants):
             print(f"Warning: no data exist for {participant} and {str(image_name).split(os.sep)[-1]}")
             continue
 
-        make_scanpath(str(image_name), df_temp, imres)
+        if use_cv2:
+            make_scanpath_cv2(image_name, df_temp, imres)
+        else:
+            make_scanpath(image_name, df_temp, imres)
 
 
 print('\n\nPlotting scanpaths took {}s to finish!'.format(time.time()-start))
