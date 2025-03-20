23fa02cf321e35d7531ef6721c35a43f15d2585a
mnystrom <marcus-nystrom@users.noreply.github.com>
2025-03-20 15:21:48 +0100

    implemented a more memory efficient version if many AOI images are used

--- demo_analyses/AOI_example/map_fixations_to_aois.py
+++ demo_analyses/AOI_example/map_fixations_to_aois.py
@@ -10,6 +10,9 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import os
 
+# Set this to True to avoid memory issues if using many AOI images
+many_aois = False
+
 # %%
 # Read files with fixations generated with the I2MC
 df_fixations = pd.read_csv('allfixations.txt', sep='\t')
@@ -25,12 +28,15 @@ for p in aoi_folder.rglob("*"):
         trial_name = str(p).split(os.sep)[-2]
         aoi_name = p.stem
 
-        # Read binary AOI image
-        temp_im = plt.imread(p)
+        if many_aois:
+            temp_im = p
+        else:
+            # Read binary AOI image
+            temp_im = plt.imread(p)
 
-        # If an RGB image, use only R-band.
-        if len(temp_im.shape) > 2:
-            temp_im = temp_im[:, :, 0]
+            # If an RGB image, use only R-band.
+            if len(temp_im.shape) > 2:
+                temp_im = temp_im[:, :, 0]
 
         # Add AOIs to dictionary
         if trial_name in image_aois:
@@ -55,8 +61,21 @@ for i, row in df_fixations.iterrows():
 
     participant = row.participant
 
-    # Find AOIs for this trial
-    aois = image_aois[trial]
+    if many_aois:
+        aoi_names = image_aois[trial]
+
+        aois = {}
+        for key in aoi_names:
+            temp_im =plt.imread(aoi_names[key])
+
+            # If an RGB image, use only R-band.
+            if len(temp_im.shape) > 2:
+                temp_im = temp_im[:, :, 0]
+
+            aois[key] = temp_im
+    else:
+        # Find AOIs for this trial
+        aois = image_aois[trial]
 
     # Position and duration of fixation
     x, y = row.xpos, row.ypos
@@ -66,7 +85,7 @@ for i, row in df_fixations.iterrows():
     hit = False
     for key in aois:
         try:
-            if aois[key][int(y), int(x)] == temp_im.max():
+            if aois[key][int(y), int(x)] == aois[key].max():
                 aoi_hits.append([row.participant, row.trial, trial_fixation_no, x, y, dur, key])
                 hit = True
         except:
