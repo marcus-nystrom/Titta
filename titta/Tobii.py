362c51febe289cd2896e1432f6e35f9039b929e0
Diederick Niehorster <dcnieho@gmail.com>
2025-08-13 11:56:27 +0200

    typo

--- titta/Tobii.py
+++ titta/Tobii.py
@@ -546,7 +546,7 @@ class myTobii(object):
 
     #%%
     def _add_to_name(self, fname, append_name=True):
-        # Add pid and path to name of calibraiton/validation images and calibration data
+        # Add pid and path to name of calibration/validation images and calibration data
         if self.settings.DATA_STORAGE_PATH.strip():
             # Add participant name to filename
             if append_name:
